af5f538b0cefcd7fe491be002b58b08cca8380e2
liujingshi-py <805746326@qq.com>
2021-04-12 10:38:08 +0800

    aaa

--- src/main/java/life/liujs/community/controller/AuthorizeController.java
+++ src/main/java/life/liujs/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -26,7 +28,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -35,7 +38,12 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_url(redirectUrl);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println("******** 用户姓名： "+ user.getName());
-        return "index";
+        if (user != null){
+            System.out.println("******** 用户姓名： "+ user.getName());
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+        }
     }
 }
